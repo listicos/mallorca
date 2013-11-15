22b941013ef2a9d6222e2149b291f526fe8382da
Ruben Velazquez Calva <ruben.velazquez.calva@gmail.com>
2013-11-15 00:15:29 -0600

    mm

--- Controllers/admin/panel.php
+++ Controllers/admin/panel.php
@@ -1,21 +1,20 @@
 <?php
 $usuario_core->validateUser();
+
+AllowRoles("Administrador, Comercial, Socio");
+
 $template = new Core_template('admin/template.php');
 $template->setAttribute('is_visible_header', true);
 $template->setAttribute('is_visible_sidebar', true);
 
+
+if(isset($_GET['id'])) {
+    $template->setAttribute('empresaId', $_GET['id']);
+}
+
+$template->setJS('admin/lista-apartamento.js');
 $template->setView('admin/apartamento/lista.php');
 $template->setTitle("Apartamentos");
-$apartamentos = getApartamentos();
-$apartamentos_array = array();
 
-foreach ($apartamentos as $key => $ap) {
-    $apartamentos_array[$key]['apartamento'] = $ap;
-    $apartamentos_array[$key]['direccion'] = getDireccion($ap->idDireccion);
-    $empresaContrato = getEmpresaContrato($ap->idEmpresaContrato);
-    $empresa = getEmpresa($empresaContrato->idEmpresa);
-    $apartamentos_array[$key]['empresa'] = $empresa;
-}
-$template->setAttribute('apartamentos', $apartamentos_array);
 echo $template->render();
 ?>
