ae634995a7a7da07f59d5d1706c699df9b7160e4
reyniergt <reyniergt@gmail.com>
2013-11-15 11:03:35 -0500

    updated admin-panel

--- Controllers/admin/panel.php
+++ Controllers/admin/panel.php
@@ -3,18 +3,5 @@ $usuario_core->validateUser();
 
 AllowRoles("Administrador, Comercial, Socio, Mallorca");
 
-$template = new Core_template('admin/template.php');
-$template->setAttribute('is_visible_header', true);
-$template->setAttribute('is_visible_sidebar', true);
-
-
-if(isset($_GET['id'])) {
-    $template->setAttribute('empresaId', $_GET['id']);
-}
-
-$template->setJS('admin/lista-apartamento.js');
-$template->setView('admin/apartamento/lista.php');
-$template->setTitle("Apartamentos");
-
-echo $template->render();
+header('Location:'.$base_url.'/admin-inicio');
 ?>
