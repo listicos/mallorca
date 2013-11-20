063104c399d7c92b78dd9934a476395f135b7036
Ruben Velazquez Calva <ruben.velazquez.calva@gmail.com>
2013-11-19 20:00:15 -0600

    123

--- Controllers/admin/inicio.php
+++ Controllers/admin/inicio.php
@@ -1,9 +1,6 @@
 <?php
 
 $usuario_core->validateUser();
-
-AllowRoles("Administrador, Socio, Reserva, Mallorca");
-
 $template = new Core_template('admin/template.php');
 $template->setAttribute('is_visible_header', true);
 $template->setAttribute('is_visible_sidebar', true);
